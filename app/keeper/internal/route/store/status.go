60a94e0ad24dd14ff7a9d06391ead5107e0ba993
Volkan Özçelik <me@volkan.io>
2024-12-27 08:50:09 -0800

    cleanup
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- app/keeper/internal/route/store/status.go
+++ app/keeper/internal/route/store/status.go
@@ -5,10 +5,14 @@
 package store
 
 import (
-	"github.com/spiffe/spike/internal/log"
 	"net/http"
+
+	"github.com/spiffe/spike/internal/log"
 )
 
 func RouteStatus(w http.ResponseWriter, r *http.Request, audit *log.AuditEntry) error {
+	// TODO: implement me.
+	// TODO: maybe nexus would want to call me before querying for shards.
+
 	return nil
 }
