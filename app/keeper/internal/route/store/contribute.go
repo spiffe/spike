37bae15c72c8a2e84771d1a6c253871c82e8c2c8
Volkan Özçelik <me@volkan.io>
2025-01-09 21:53:03 -0800

    remove logging sensitive info
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- app/keeper/internal/route/store/contribute.go
+++ app/keeper/internal/route/store/contribute.go
@@ -53,9 +53,7 @@ func RouteContribute(
 
 	// Store decoded shard in the map.
 	state.Shards.Store(id, decodedShard)
-
-	// TODO: DO NOT LOG THE SHARD!!!!!!!!!!! -- check other routes too.
-	log.Log().Info(fName, "msg", "Shard stored", "id", id, "shard", decodedShard)
+	log.Log().Info(fName, "msg", "Shard stored", "id", id)
 
 	responseBody := net.MarshalBody(reqres.ShardContributionResponse{}, w)
 
