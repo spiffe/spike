c04b88fa8afb45a7ebd32adeced2450f1244280b
Volkan Özçelik <me@volkan.io>
2025-04-10 21:38:42 -0700

    Typo fix.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/env.sh
+++ hack/env.sh
@@ -11,7 +11,8 @@
 # debugging easier.
 
 export SPIKE_NEXUS_MAX_SECRET_VERSIONS="10"
-export SPIKE_NEXUS_BACKEND_STORE="sqlite"
+# export SPIKE_NEXUS_BACKEND_STORE="sqlite"
+export SPIKE_NEXUS_BACKEND_STORE="memory"
 export SPIKE_NEXUS_TLS_PORT=":8553"
 export SPIKE_NEXUS_SHA_HASH_LENGTH="32"
 export SPIKE_NEXUS_PBKDF2_ITERATION_COUNT="600000"
@@ -38,5 +39,5 @@ export SPIKE_KEEPER_TLS_PORT=":8443"
 
 export SPIKE_SYSTEM_LOG_LEVEL="debug"
 
-expoort SPIKE_TRUST_ROOT="spike.ist"
+export SPIKE_TRUST_ROOT="spike.ist"
 export SPIKE_NEXUS_API_URL="https://localhost:8553"
