51e5a0b8ae07725e38590ac4794c12f6fcad8922
Volkan Özçelik <me@volkan.io>
2025-05-19 22:42:02 -0700

    SPIRE installation.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/helm-spire-install.sh
+++ hack/helm-spire-install.sh
@@ -4,18 +4,42 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
+set -e  # Exit on any error
+
 # Enable alias expansion in non-interactive shells
 shopt -s expand_aliases
 
 source ./hack/aliases.sh
 
-helm repo add spiffe https://spiffe.github.io/helm-charts/
+# Add Helm repository if it doesn't exist
+if ! helm repo list | grep -q "^spiffe\s"; then
+    echo "Adding spiffe Helm repository..."
+    helm repo add spiffe https://spiffe.github.io/helm-charts/
+else
+    echo "spiffe Helm repository already exists."
+fi
+
+# Update Helm repositories
+echo "Updating Helm repositories..."
 helm repo update
 
-# Create a namespace for SPIRE
-kubectl create namespace spire-system
+# Create namespace if it doesn't exist
+if ! kubectl get namespace spire-system &> /dev/null; then
+    echo "Creating spire-system namespace..."
+    kubectl create namespace spire-system
+else
+    echo "spire-system namespace already exists."
+fi
 
-# Install SPIRE using Helm
-helm install spire spiffe/spire --namespace spire-system \
- -f ./config/spire/helm/values.yaml
+# Check if SPIRE is already installed
+if helm list -n spire-system | grep -q "^spire\s"; then
+    echo "SPIRE is already installed. Upgrading with the latest values..."
+    helm upgrade spire spiffe/spire --namespace spire-system \
+         -f ./config/spire/helm/values.yaml
+else
+    echo "Installing SPIRE..."
+    helm install spire spiffe/spire --namespace spire-system \
+         -f ./config/spire/helm/values.yaml
+fi
 
+echo "SPIRE setup completed successfully."
--- hack/helm-spire-uninstall.sh
+++ hack/helm-spire-uninstall.sh
@@ -4,3 +4,15 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
+# Enable alias expansion in non-interactive shells
+shopt -s expand_aliases
+
+source ./hack/aliases.sh
+
+helm uninstall spire -n spire-system
+
+kubectl delete pvc -n spire-system -l app.kubernetes.io/instance=spire
+
+kubectl delete clusterspiffeids --all
+
+echo "SPIRE uninstalled."
