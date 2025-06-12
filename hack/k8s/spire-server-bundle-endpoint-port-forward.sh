0aee92ecdde3ae97e8d1971dba656fd6e5c8d181
Volkan Özçelik <me@volkan.io>
2025-06-11 21:46:47 -0700

    WIP: port forward keepers.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/k8s/spire-server-bundle-endpoint-port-forward.sh
+++ hack/k8s/spire-server-bundle-endpoint-port-forward.sh
@@ -4,4 +4,6 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
+# TODO: move all these demo-related scripts to a "demo" folder maybe.
+
 kubectl -n spire-server port-forward svc/spiffe-server 8443:8443 --address=0.0.0.0
