1ec591effeff6343882f1c5c74e8d3786adfab0c
Volkan Özçelik <me@volkan.io>
2025-06-21 18:57:00 -0700

    minor.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- examples/federation/spire-server-bundle-endpoint-port-forward.sh
+++ examples/federation/spire-server-bundle-endpoint-port-forward.sh
@@ -4,6 +4,4 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
-# TODO: move all these demo-related scripts to a "demo" folder maybe.
-
 kubectl -n spire-server port-forward svc/spiffe-server 8443:8443 --address=0.0.0.0
