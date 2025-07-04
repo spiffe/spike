a8806bc800aba7e012dba2d3ec2119ca7510211a
Volkan Özçelik <me@volkan.io>
2025-07-04 15:31:57 -0700

    Add minikube install script.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

---
+++ hack/k8s/minikube-install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#    \\ SPIKE: Secure your secrets with SPIFFE.
+#  \\\\\ Copyright 2024-present SPIKE contributors.
+# \\\\\\\ SPDX-License-Identifier: Apache-2.0
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
