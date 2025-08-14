4f34fb6eb7dc881a64d805d393d0d4a225b0b68a
Volkan Özçelik <me@volkan.io>
2025-08-14 23:35:51 +0300

    minor.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/docker/minikube-load-images.sh
+++ hack/docker/minikube-load-images.sh
@@ -7,21 +7,14 @@
 docker tag spike-bootstrap:dev localhost:5000/spike-bootstrap:dev
 minikube image load localhost:5000/spike-bootstrap:dev
 
-docker tag spike-bootstrap:dev localhost:5000/spike-bootstrap:dev
-minikube image load localhost:5000/spike-bootstrap:dev
+docker tag spike-demo:dev localhost:5000/spike-demo:dev
+minikube image load localhost:5000/spike-demo:dev
 
-docker tag spike-bootstrap:dev localhost:5000/spike-bootstrap:dev
-minikube image load localhost:5000/spike-bootstrap:dev
+docker tag spike-pilot:dev localhost:5000/spike-pilot:dev
+minikube image load localhost:5000/spike-pilot:dev
 
-docker tag spike-bootstrap:dev localhost:5000/spike-bootstrap:dev
-minikube image load localhost:5000/spike-bootstrap:dev
+docker tag spike-nexus:dev localhost:5000/spike-nexus:dev
+minikube image load localhost:5000/spike-nexus:dev
 
-docker tag spike-bootstrap:dev localhost:5000/spike-bootstrap:dev
-minikube image load localhost:5000/spike-bootstrap:dev
-
-minikube image load spike-demo:dev
-minikube image load spike-pilot:dev
-minikube image load spike-nexus:dev
-minikube image load spike-keeper:dev
-
-
+docker tag spike-keeper:dev localhost:5000/spike-keeper:dev
+minikube image load localhost:5000/spike-keeper:dev
