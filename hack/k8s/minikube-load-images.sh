ab8bbd15c45da01df9e63e56004998204cefa375
Volkan Özçelik <me@volkan.io>
2025-09-14 10:17:30 -0700

    Fix bootstrap crashes
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/k8s/minikube-load-images.sh
+++ hack/k8s/minikube-load-images.sh
@@ -10,19 +10,19 @@ echo "Loaded spike-bootstrap:dev."
 
 docker tag spike-demo:dev localhost:5000/spike-demo:dev
 minikube image load localhost:5000/spike-demo:dev
-echo "Loaded spike-demo:dev"
+echo "Loaded spike-demo:dev."
 
 docker tag spike-pilot:dev localhost:5000/spike-pilot:dev
 minikube image load localhost:5000/spike-pilot:dev
-echo "Loaded spike-pilot:dev"
+echo "Loaded spike-pilot:dev."
 
 docker tag spike-nexus:dev localhost:5000/spike-nexus:dev
 minikube image load localhost:5000/spike-nexus:dev
-echo "Loaded spike-nexus:dev"
+echo "Loaded spike-nexus:dev."
 
 docker tag spike-keeper:dev localhost:5000/spike-keeper:dev
 minikube image load localhost:5000/spike-keeper:dev
-echo "Loaded spike-keeper:dev"
+echo "Loaded spike-keeper:dev."
 echo
 
 echo "All images loaded to the local Minikube registry."
