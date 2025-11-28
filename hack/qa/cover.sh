82f6ed36476cfb6392e9e2d8dc9a6fa381682343
Volkan Özçelik <me@volkan.io>
2025-11-28 15:32:15 -0800

    add coverage.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/qa/cover.sh
+++ hack/qa/cover.sh
@@ -4,8 +4,12 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
+go build ./...
+
 # FIXME: Remove -p 1 flag once issue with concurrent test isolation is resolved
-go test -race -shuffle=on -coverprofile=coverage.txt -p 1 ./...
+# Intentionally not using `-race` since all we care about is generating a
+# coverage report.
+go test -shuffle=on -coverprofile=coverage.txt -p 1 ./...
 
 go tool cover -html=coverage.txt -o=coverage.html
 
